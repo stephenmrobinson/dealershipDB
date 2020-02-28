e3010751f7fe465d3c41ef589840578fc774fc8f
Stephen M Robinson <cs.smrobinson@gmail.com>
2020-02-28 17:44:14 -0500

    Added remaining SQL insert scripts

---
+++ developer/CUSTOMERS.sql
@@ -0,0 +1,31 @@
+/*CUSTOMERS&*/
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (01, 'Gerald', 'Arbor', '1 Glendwood Ave.', 'Carmine', 'IL', 47824, 78923942, 423, 'Facebook');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (02, 'Gabby', 'Baxter', '5 Glendwood Ave.', 'Carmine', 'IL', 47824, 78923943, 423, 'Facebook');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (03, 'April', 'Carlyle', '10 Simple Ave.', 'Carmine', 'IL', 47824, 78923944, 423, 'Twitter');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (04, 'Stanley', 'Dabbuk', '15 Simple Ave.', 'Carmine', 'IL', 47824, 78923542, 423, 'Twitter');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (05, 'Kim', 'Edwards', '17 Graveyard Ave.', 'Carmine', 'IL', 47824, 78923972, 423, 'Craigslist');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (06, 'Tan', 'Fallon', '19 Glendwood Ave.', 'Carmine', 'IL', 47824, 78923882, 423, 'Craigslist');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (07, 'Sandford', 'Gentry', '56 Graveyard Ave.', 'Carmine', 'IL', 47824, 78921942, 423, 'Billboard');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (08, 'Ruth', 'Hyde', '16 Graveyard Ave.', 'Carmine', 'IL', 47824, 78923977, 423, 'Billboard');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (09, 'Bernard', 'Ingle', '13 Tupelo Ave.', 'Carmine', 'IL', 47824, 78923992, 423, 'Facebook');
+
+INSERT INTO CARCUSTOMER (custid, firstname, lastname, street, city, state, zip, phonenumber, areacode, adseen)
+VALUES (10, 'Stephen', 'Jackson', '11 Tupelo Ave.', 'Carmine', 'IL', 47824, 78923947, 423, 'Newspaper');
---
+++ developer/SALESPERSON.sql
@@ -0,0 +1,33 @@
+/* EMPLOYEE RECORDS */
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1234567, 'Sal', 'Ford', '123 Lotus Ave.', 'Carmine', 'IL', 47824, 423, 7891234, '21-MAY-2005');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1234589, 'Sally', 'Faraday', '12 Charmichael Ave.', 'Carmine', 'IL', 47824, 423, 7891324, '16-AUG-2008');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1234560, 'Carl', 'Hemmit', '1 Champion Blvd.', 'Carmine', 'IL', 47824, 423, 7884234, '11-FEB-2003');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1235567, 'Garret', 'Shellby', '23 Lotus Ave.', 'Carmine', 'IL', 47824, 423, 7891220, '25-MAR-2005');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1234767, 'Timmy', 'Tomberlin', '3 Olive St.', 'Carmine', 'IL', 47824, 423, 7891111, '21-MAY-2009');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1236567, 'Cam', 'Peralta', '11 Lotus Ave.', 'Carmine', 'IL', 47824, 423, 7891554, '21-MAY-2012');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1238567, 'Bently', 'Charmington', '55 English Ave.', 'Carmine', 'IL', 47824, 423, 7891288, '21-SEP-2015');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1234577, 'Charles', 'Greig', '125 Lotus Ave.', 'Carmine', 'IL', 47824, 423, 7901234, '21-OCT-2017');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1834567, 'Tupelo', 'Flowerchild', '33 Creeping Blvd.', 'Carmine', 'IL', 47824, 423, 7901211, '21-AUG-2019');
+
+INSERT INTO CARSALESPERSON (empid, firstname, lastname, street, city, state, zip, areacode, phonenumber, datehired)
+VALUES (1231111, 'Fredrick', 'Fallon', '67 Champion Blvd.', 'Carmine', 'IL', 47824, 423, 7895634, '21-SEP-2015');
+
+    
